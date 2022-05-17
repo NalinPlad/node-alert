1bbaf26a1a483081d4ba97578b5488f34574594e
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-05-17 09:56:49 -0700

    🚀 Added buttons to class

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-const execa = require('execa');
+const execa = require("execa");
 
-async function roc(command){
-    await execa('osascript', ['-e', command])
+async function roc(command) {
+  return await execa("osascript", ["-e", command]).stdout;
 }
 
-class popup_options{
-    constructor(
-        buttons,
-        icon,
-        giveupafter
-    ){
-        this.buttons = buttons;
-        this.icon = icon;
-        this.giveupafter = giveupafter;
-    }
+class popup_options {
+  constructor(buttons, defaultbutton, cancelbutton, icon, giveupafter) {
+    this.buttons = buttons;
+    this.defaultbutton = defaultbutton;
+    this.cancelbutton = cancelbutton;
+    this.icon = icon;
+    this.giveupafter = giveupafter;
+  }
 }
 
-const opts = new popup_options(['yes','no'],'caution',5)
+async function dialog(message, options) {
+  let osa = "display dialog " + '"' + message + '"';
 
-async function dialog(message,options){
-    await roc(`display dialog '${message}'`)
+  if (options.buttons.length > 1) {
+    osa +=
+      ' buttons {"' +
+      options.buttons[0] +
+      '","' +
+      options.buttons[1] +
+      '","' +
+      options.buttons[2] +
+      '"}';
+  } else if (options.buttons.length == 1) {
+    osa += ' buttons {"' + options.buttons[0] + '"}';
+  }
+  await roc(osa);
 }
 
-dialog('123321',opts)
+const opts = new popup_options(["Hi!", "No!"], "Hi!", "No!", "caution", 5);
+dialog("Hello", opts);
