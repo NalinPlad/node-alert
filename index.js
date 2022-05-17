3a5d16b45595512c5b659d03d1ee3cc63c9c8727
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-05-17 08:56:06 -0700

    added popup_options class

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ async function roc(command){
     await execa('osascript', ['-e', command])
 }
 
-roc('display dialog "Do, or do not. There is no try."')
+class popup_options{
+    constructor(
+        buttons,
+        icon,
+        giveupafter
+    )
+}
+
+const opts = new popup_options(['yes','no'],'caution',5)
+
+async function dialog(message,options){
+    await roc(`display dialog '${message}'`)
+}
+
+dialog('123321',opts)
